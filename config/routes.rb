c8362402d65eb6f5b849ebb2e511d3d1cce8058d
Ccastle <naresh.dwivedi@wwindia.com>
2015-08-06 03:17:33 +0530

    Added landing page with the form for subscribing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Sogweb::Application.routes.draw do
-  root 'email_subscriptions#new'
+  root "home#index"
+  get 'email_subscriptions/new'
   post "email_subscriptions/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
